3a8546b73b47845c987fcda1c20411971c0da59f
José Alberto Leiva Obando <jleiva@growthaccelerationpartners.com>
2024-04-18 10:16:48 -0600

    Update key-management.psm1
    Move help comment

--- Installer/key-management.psm1
+++ Installer/key-management.psm1
@@ -2,39 +2,7 @@
 # Licensed to the Ed-Fi Alliance under one or more agreements.
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
-
-<#
-.SYNOPSIS
-    Generates a new AES key.
-
-.DESCRIPTION
-    The New-AESKey function creates a new AES key using the .NET class System.Security.Cryptography.Aes.
-    The key size is set to 256 bits, and the key is generated using the GenerateKey method.
-    The key is then converted to a Base64 string for easier handling.
-
-.PARAMETER
-    This function does not take any parameters.
-
-.EXAMPLE
-    PS C:\> New-AESKey
-
-    This command generates a new AES key and outputs it as a Base64 string.
-
-.INPUTS
-    None. You cannot pipe objects to New-AESKey.
-
-.OUTPUTS
-    System.String. New-AESKey returns the new AES key as a Base64 string.
-
-.NOTES
-    The AES key is generated in memory and not saved to disk by this function.
-    Be sure to capture the output of this function if you need to use the key later.
-#>
-
 function New-AESKey {
-    [CmdletBinding(SupportsShouldProcess = $true)]
-    [OutputType([System.String])]
-    param()
     <#
         .SYNOPSIS
             Generates and Base64-encodes a 256 bit key appropriate for use with AES encryption.
@@ -45,6 +13,9 @@ function New-AESKey {
 
             # Copy the output and paste into the appsettings file.
     #>
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    [OutputType([System.String])]
+    param()
     if ($PSCmdlet.ShouldProcess("AES Key", "New")) {
         $aes = [System.Security.Cryptography.Aes]::Create()
         $aes.KeySize = 256
